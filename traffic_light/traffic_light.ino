1d7dc7184412a0d68cc8110548a8c678a43fbb23
Adetunji Onafuwa <adetunjionafuwa@gmail.com>
2019-06-26 07:10:41 +0200

    Assign light values for various LED

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -1,3 +1,13 @@
+//Light one variables
+int red1 = 10;
+int yellow1 = 9;
+int green1 = 8;
+
+//Light two variables
+int red2 = 13;
+int yellow2 = 12;
+int green2 = 11;
+
 void setup() {
   // put your setup code here, to run once:
 
